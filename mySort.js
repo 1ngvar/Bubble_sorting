3e7cccbee03af842ca7b426db467666019a725e6
Mona Lisa <1ngvar@pm.me>
2020-09-04 16:34:02 +0300

    It's alive! ALIVE! :moyai:

--- mySort.js
+++ mySort.js
@@ -1,46 +1,48 @@
-const inputArray = prompt('Для построения графика введите числа, разделённые запятыми. \nНечисловые значения будут приравнены к 0.',
-    [2, 1, 3, 3, 2, 4, 3, 6, 5, 4, 7, 8, 6, 5, 4, 44, 33, 4, 2, 56]).split(',')
+/*
+    Sweetalert.js
+*/
+
+Swal.fire({
+    input: 'text',
+    inputValue: '63, 1, 3, 20, 74, 4, 35, 6, 5, 28, 7, 8, 6, 5, 4, 44, 33, 4, 2, 56',
+    html: 'Введите числа, разделённые запятыми.<br/> Нечисловые значения будут приравнены к 0.',
+    icon: 'info',
+    confirmButtonText: 'Готово!',
+    backdrop: `
+    rgba(0,0,123,0.4)
+    url("img/nyan-cat.gif")
+    left top
+    no-repeat
+  `
+}).then((result) => { if (result.value) { renderArray(result.value) } });
+
+
+
+/*
+    Variables declaration
+
+const inputArray = prompt('Для построения графика введите числа, разделённые запятыми.\n Нечисловые значения будут приравнены к 0.',
+    [63, 1, 3, 20, 74, 4, 35, 6, 5, 28, 7, 8, 6, 5, 4, 44, 33, 4, 2, 56]).split(',')
     .map(x=> +x || 0 )
+*/
+const figure = document.getElementsByTagName("figure")
+const allDivs = document.getElementsByTagName("div")
+const title = document.getElementById("title")
 
 
-bubbleSorting(inputArray)
+// createFigure(inputArray)
 
 
+/*
+    Functions declaration
+*/
 
+function renderArray(inputString) {
+    window.inputArray = inputString.split(',').map(x=> +x || 0 );
+    createFigure(inputArray);
+    console.log("inputString: " +  inputString);
 
-// Functions declarations
-
-function createFigure(array) {
-
-    let figure = document.getElementsByTagName("figure")
-
-    // Creating a div for every array element
-    for (let i = 0; i < array.length; i++) {
-
-        let bar = createDivBar(array[i])
-
-        // Setting it's id according to the element's position in an array
-        bar.setAttribute("id", i)
-
-        // Adding a div bar to a <figure>
-        figure[0].appendChild(bar)
-    }
 }
-
-
-function createDivBar(arrayItem) {
-
-    let div = document.createElement("div")
-
-    // Printing array numbers over their respective bars
-    div.innerText = arrayItem
-
-    // Setting div's height according to the element's value multiplied by 5 for better visibility
-    div.style.height = arrayItem * 5 + "px"
-
-    return div
-}
-
 
 function bubbleSorting(arr) {
     let temp
@@ -58,3 +60,67 @@ function bubbleSorting(arr) {
     }
     createFigure(arr)
 }
+
+
+function createFigure(array) {
+
+    // Creating a div for every array element
+    for (let i = 0; i < array.length; i++) {
+
+        let bar = createDivBar(array[i])
+
+        bar.onclick = zeroOutDivs
+
+        figure[0].appendChild(bar)
+    }
+
+    // Fading the title in
+    setTimeout(() => {
+        title.style.opacity = 1
+        // title.style.opacity = 0 ? title.style.opacity = 1 : title.style.opacity = 0
+    }, 3000);
+}
+
+
+function createDivBar(arrayItem) {
+
+    let div = document.createElement("div")
+
+    // Printing array numbers over their respective bars
+    div.innerText = arrayItem
+
+    // Creating initial divs to modify them later
+    div.style.height = 0
+    div.style.opacity = 0
+
+
+    setTimeout(() => {
+        // Setting div's height according to the element's value multiplied by 5 for better visibility
+        div.style.height = arrayItem * 5 + "px"
+        div.style.opacity = 1
+    }, 100)
+
+    return div
+}
+
+
+function zeroOutDivs() {
+
+    for (div of allDivs) { div.style.height = "11px"; }
+
+    // Fading out the title
+    setTimeout(() => { title.style.opacity = 0  }, 1000);
+
+    // Fading out old divs
+    setTimeout(() => {  for (div of allDivs) { div.style.opacity = "0"; }  }, 2000);
+
+
+    // Deleting them
+    setTimeout(() => figure[0].innerHTML = '', 3000);
+
+    // Creating a brand new Figure
+    setTimeout(() => { bubbleSorting(inputArray) }, 4000)
+}
+
+
+
